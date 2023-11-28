d0ec5fac68ebc07ef30be5904c7341055c509508
law <law.liu@token.im>
2023-11-28 20:54:12 +0800

    support commonjs

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import commonjs from 'vite-plugin-commonjs';
 import top from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [top(), commonjs(), wasm(), react()],
+  plugins: [top(), wasm(), react()],
   resolve: {
     alias: {
       crypto: 'crypto-browserify',
@@ -15,21 +14,11 @@ export default defineConfig({
       stream: 'stream-browserify',
     },
   },
-  // resolve: {
-  //   alias: {
-  //     http: require.resolve('rollup-plugin-node-builtins'),
-  //     path: require.resolve('rollup-plugin-node-builtins'),
-  //     fs: require.resolve('rollup-plugin-node-builtins'),
-  //     os: require.resolve('rollup-plugin-node-builtins'),
-  //     tslib: require.resolve('rollup-plugin-node-builtins'),
-  //     child_process: require.resolve('rollup-plugin-node-builtins'),
-  //     crypto: require.resolve('rollup-plugin-node-builtins'),
-  //     stream: require.resolve('rollup-plugin-node-builtins'),
-  //     https: require.resolve('rollup-plugin-node-builtins'),
-  //     http2: require.resolve('rollup-plugin-node-builtins'),
-  //     process: require.resolve('rollup-plugin-node-builtins'),
-  //   },
-  // },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
